7eee212d912ac7236f4d34eb9c209cf5e6816bc0
battez <lukebarker@gmail.com>
2018-10-22 11:17:21 +0100

    add several solutions for intermediate problems

--- exs-interm-algs.js
+++ exs-interm-algs.js
@@ -1,10 +1,14 @@
+// See problems here:
+// https://learn.freecodecamp.org
+// /javascript-algorithms-and-data-structures
+// /intermediate-algorithm-scripting
 
 /*
 DNA pair all chars in a string
-The DNA strand is missing the pairing element. Take each character, 
+The DNA strand is missing the pairing element. Take each character,
 get its pair, and return the results as a 2d array.
 
-Base pairs are a pair of AT and CG. Match the missing element to the 
+Base pairs are a pair of AT and CG. Match the missing element to the
 provided character.
 
 Return the provided character as the first element in each array.
@@ -42,7 +46,7 @@ function pairElement(str) {
 	}
 
 	let pairs = parts.map(halfpair => getOtherHalf(halfpair));
-  	return pairs;
+  return pairs;
 }
 
 pairElement("GCG");
@@ -54,7 +58,7 @@ function fearNotLetter(str) {
 	for (var i = 0; i < str.length; i++) {
 		
 		if(i !== 0){
-			if(str.charCodeAt(i) - last === 1) 
+			if(str.charCodeAt(i) - last === 1)
 			{
 				last = str.charCodeAt(i);
 				continue;
@@ -70,13 +74,13 @@ function fearNotLetter(str) {
 
 	}
 
-  	return undefined;
+  return undefined;
 }
 
 fearNotLetter("abce");
 
 /*
-Write a function that takes two or more arrays and returns a new array of 
+Write a function that takes two or more arrays and returns a new array of
 unique values in the order of the original provided arrays.
 
 In other words, all values present from all arrays should be included in
@@ -88,7 +92,7 @@ The unique numbers should be sorted by their original order, but the final
  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
  uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
  uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
- uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) 
+ uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])
  should return [1, 2, 3, 5, 4, 6, 7, 8].
 */
 function uniteUnique(arr) {
@@ -103,7 +107,7 @@ function uniteUnique(arr) {
 		});
 	  
 	}
-  	return Array.from(result);
+  return Array.from(result);
 }
 
 /*
@@ -116,7 +120,7 @@ convertHTML("Dolce & Gabbana") should return Dolce &​amp; Gabbana.
 convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &​lt;
  Pizza &​lt; Tacos.
 convertHTML("Sixty > twelve") should return Sixty &​gt; twelve.
-convertHTML('Stuff in "quotation marks"') should return Stuff in 
+convertHTML('Stuff in "quotation marks"') should return Stuff in
 &​quot;quotation marks&​quot;.
 convertHTML("Schindler's List") should return Schindler&​apos;s List.
 */
@@ -132,14 +136,14 @@ convertHTML("Dolce & Gabbana");
 
 /*
 
-Given a positive integer num, return the sum of all odd Fibonacci numbers 
+Given a positive integer num, return the sum of all odd Fibonacci numbers
 that are less than or equal to num.
 
-The first two numbers in the Fibonacci sequence are 1 and 1. Every additional 
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional
 number in the sequence is the sum of the two previous numbers. The first six
  numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
 
-For example, sumFibs(10) should return 10 because all odd Fibonacci numbers 
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers
 less than or equal to 10 are 1, 1, 3, and 5.
 
 */
@@ -168,8 +172,8 @@ function sumFibs(num) {
 /*
 Sum all the prime numbers up to and including the provided number.
 
-A prime number is defined as a number greater than one and having only 
-two divisors, one and itself. For example, 2 is a prime number because it's 
+A prime number is defined as a number greater than one and having only
+two divisors, one and itself. For example, 2 is a prime number because it's
 only divisible by one and two.
 
 The provided number may not be a prime.
@@ -181,11 +185,11 @@ sumPrimes(977) should return 73156.
 
 function sumPrimes(num) {
 
-	let primes = [2, 3]; 
+	let primes = [2, 3];
 
 	for (let i = 2; i <= num; i++) {
 
-		// keep track of whether prime. 
+		// keep track of whether prime.
 		// At end use to decide to push() or not
 		let prime = true;
 		
@@ -211,7 +215,7 @@ function sumPrimes(num) {
 	}
 	
 	const reducer = (total, value) => total + value;
-  	return primes.reduce(reducer);
+  return primes.reduce(reducer);
 }
 
 sumPrimes(10);
@@ -224,14 +228,14 @@ sumPrimes(10);
  *
 
  Find the smallest common multiple of the provided parameters that can be
-  evenly divided by both, as well as by all sequential numbers in the range 
+  evenly divided by both, as well as by all sequential numbers in the range
   between these parameters.
 
 The range will be an array of two numbers that will not necessarily be in
  numerical order.
 
 For example, if given 1 and 3, find the smallest common multiple of both 1 and
- 3 that is also evenly divisible by all numbers between 1 and 3. 
+ 3 that is also evenly divisible by all numbers between 1 and 3.
  The answer here would be 6
 smallestCommons([1, 5]) should return a number.
 smallestCommons([1, 5]) should return 60.
@@ -252,7 +256,7 @@ function smallestCommons(arr) {
 
 	while (found === false) {
 
-		// key to efficiency is increase by largest factor each pass 
+		// key to efficiency is increase by largest factor each pass
 		curr += arr[1];
 		
 
@@ -280,7 +284,7 @@ function smallestCommons(arr) {
 		}
 	}
 
-  	return found;
+  return found;
 		
 }
 
@@ -288,7 +292,244 @@ function smallestCommons(arr) {
 smallestCommons([1,5]);
 
 
-// ================== 
+
+
+/*
+Given the array arr, iterate through and
+remove each element starting from the first element
+(the 0 index) until the function func returns true
+when the iterated element is passed through it.
+
+Then return the rest of the array once the condition
+is satisfied, otherwise, arr should be returned as
+an empty array.
+*/
+function dropElements(arr, func) {
+
+  for(var i=0; i < arr.length; i++){
+
+    if (func(arr[i])) {
+      break;
+    }
+  }
+
+  return arr.slice(i);
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
+
+
+// Flatten a nested array. You must account for varying levels of nesting.
+
+function steamrollArray(arr) {
+  let result = [];
+  
+  for(var i = 0; i < arr.length; i++) {
+  	
+  	// Use recursion if we find inner is an array.
+    if(! Array.isArray(arr[i])){
+    	console.log('Returning', arr[i]);
+    	
+      // add to this particular result stack
+      result.push(arr[i]);
+    }
+    else {
+    	// go another level deeper...
+      result = result.concat(steamrollArray(arr[i]));
+    
+    }
+    
+  }
+	
+	console.log('result',result);
+  return result;
+
+}
+
+
+// Return an English translated sentence of the passed binary string.
+// The binary string will be space separated.
+// use: ;
+function binaryAgent(str) {
+	let result = '';
+	
+	let arr = str.split(' ');
+	
+	result = arr.map(letter => String.fromCharCode(parseInt(letter, 2))).join('');
+	
+  return result;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 " +
+"00100000 01100010 01101111 01101110 01100110 01101001 01110010"
++ " 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+/*
+Check if the predicate (second argument)
+is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array
+collection of objects. The predicate pre will be
+an object property and you need to return true if its
+value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that
+translate to true when evaluated in a Boolean context.
+
+*/
+function truthCheck(collection, pre) {
+  for (let i=0; i < collection.length; i++) {
+  	
+      let obj = collection[i];
+
+      if(obj[pre]) {
+        continue;
+      }
+      else {
+        return false;
+      }
+  }
+  // all fine
+  return true;
+}
+
+/*
+Create a function that sums two arguments together.
+If only one argument is provided, then return a function
+that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and
+addTogether(2) should return a function.
+
+Calling this returned function with a single argument
+will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+*/
+function addTogether(x, z=0){
+	
+	if(typeof(x) !== "number" || typeof(z) !== "number") {
+		return undefined;
+	}
+	
+	if(z !== 0) {
+		// we have both args present:
+		return x + z;
+		
+	}
+	else {
+		// return fn to sum arguments (currying practise)
+  	return (y) => {
+  		
+  		// still need to validate here in case of partial arguments
+  		// TODO: Refactor as this smells redundant!
+    	if(typeof(y) !== "number") {
+		    return undefined;
+	    }
+	    
+	    else {
+	    	return x + y;
+	    }
+      
+    
+  	};
+	}
+
+}
+
+addTogether(2,3);
+
+/*
+Fill in the object constructor with the following methods below:
+
+getFirstName() getLastName() getFullName()
+setFirstName(first) setLastName(last) setFullName(firstAndLast)
+Run the tests to see the expected output for each method.
+
+The methods that take an argument must accept only one argument
+and it has to be a string.
+
+These methods must be the only available means of interacting
+with the object.
+
+*/
+let Person = function(firstAndLast) {
+	let fullName;
+	
+  // Complete the method below and implement the others similarly
+  this.setFullName = function(firstAndLast) {
+    return fullName = firstAndLast.split(' ');
+  };
+  this.setLastName = function(last) {
+    return fullName[1] = last;
+  };
+  this.setFirstName = function(first) {
+    return fullName[0] = first;
+  };
+  this.getFullName = function() {
+    return fullName.join(' ');
+  };
+  this.getLastName = function() {
+    return fullName[1];
+  };
+  this.getFirstName = function() {
+    return fullName[0];
+  };
+  
+  this.setFullName(firstAndLast);
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+
+/*
+
+Return a new array that transforms the elements' average altitude
+into their orbital periods (in seconds).
+
+The array will contain objects in the format
+{name: 'name', avgAlt: avgAlt}.
+
+
+The values should be rounded to the nearest whole number.
+The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers,
+and the GM value of earth is 398600.4418 km3s-2. */
+function orbitalPeriod(arr) {
+  
+  // sqrt (orbital radius^3 / GM) * 2PI (in seconds)
+  // orbital radius is simply semi-major axis. altitude + earths radius
+  function calcPeriod(obj) {
+  	const GM = 398600.4418;
+  	const earthRadius = 6367.4447;
+  	const altitude = obj.avgAlt;
+  	delete obj.avgAlt;
+  	
+  	// semi-major axis
+  	let a = altitude + earthRadius;
+  	
+  	// formula for an orbital period
+  	obj.orbitalPeriod = Math.sqrt(Math.pow(a, 3) / GM) * 2 * Math.PI;
+  	
+  	//console.log(Math.round(obj.orbitalPeriod));
+  	obj.orbitalPeriod = Math.round(obj.orbitalPeriod);
+  	
+  	return obj;
+  }
+  
+  return arr.map(obj => calcPeriod(obj));
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+
+// ==================
 
 // sum start and end numbers and all between them.
 // i.e. sumAll([10, 5]) should return 45.
@@ -456,7 +697,7 @@ spinalCase('This Is Spinal Tap');
 /*
 Translate the provided string to pig latin.
 
-Pig Latin takes the first consonant (or consonant cluster) of an English word, 
+Pig Latin takes the first consonant (or consonant cluster) of an English word,
 moves it to the end of the word and suffixes an "ay".
 
 
@@ -480,7 +721,7 @@ function translatePigLatin(str) {
 		console.log('no vowels');
 		return str + 'ay';
 
-	} 
+	}
 	else {
 		// main swapping for rest of cases:
 		const pattern = /^([^aeiou]+)(.+)/;
@@ -489,7 +730,7 @@ function translatePigLatin(str) {
 		if (result.substr(0,1) !== str.substr(0,1)) {
 			console.log('not vowel start');
 			result += 'ay';
-		}	
+		}
 		else {
 			console.log('vowel start');
 			result += 'way';
@@ -504,7 +745,7 @@ translatePigLatin("consonant");
 
 /*
 
-Perform a search and replace on the sentence using the arguments provided 
+Perform a search and replace on the sentence using the arguments provided
 and return the new sentence.
 
 First argument is the sentence to perform the search and replace on.
@@ -514,7 +755,7 @@ Second argument is the word that you will be replacing (before).
 Third argument is what you will be replacing the second argument with (after).
 
 Note
-Preserve the case of the first character in the original word when you are 
+Preserve the case of the first character in the original word when you are
 replacing it. For example if you mean to replace the word "Book" with the word
  "dog", it should be replaced as "Dog"
 
@@ -522,19 +763,19 @@ replacing it. For example if you mean to replace the word "Book" with the word
 function myReplace(str, before, after) {
 
 	// add this to ensure multiple matches
-	const pattern = new RegExp(before, 'gi'); 
+	const pattern = new RegExp(before, 'gi');
 
 	// handle the Note case given:
 	const isLowerCase = function(txt) {
 		return txt == txt.toLowerCase() && txt != txt.toUpperCase();
-	}  
+	}
 	
 	// define a replacer callback
 	const replacer = function (match) {
 		if (isLowerCase(match.substr(0,1))) {
 			return after;
-		} 
-		else {	
+		}
+		else {
 			return after.substr(0,1).toUpperCase() + after.substr(1);
 		}
 	}
