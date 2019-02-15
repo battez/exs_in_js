86c084919d13b9729f9fa548c5698724f84c341f
battez <lukebarker@gmail.com>
2019-02-15 10:04:03 +0000

    add solution for contig. sum problem to end file

--- exs-code-prep.js
+++ exs-code-prep.js
@@ -312,3 +312,40 @@ function findEvenIndex(arr)
 }
 
 
+/*
+The maximum sum subarray problem consists
+in finding the maximum sum of a contiguous subsequence
+in an array or list of integers:
+
+
+Easy case is when the list is made up of only
+positive numbers and the maximum sum is the sum of the whole
+array. If the list is made up of only negative numbers,
+return 0 instead.
+
+Empty list is considered to have zero greatest sum.
+
+Note that the empty list or array is also a valid sublist/subarray.
+*/
+
+let maxSequence = function(arr){
+  let maxValue = 0;
+  let currentTotal = 0;
+  
+  // nested loop should give us exhaustive way to get maximum:
+  for (let i = 0; i < arr.length; i++) {
+  	
+  	for (let j = i + 1; j <= arr.length; j++) {
+  		
+  		currentTotal = arr.slice(i, j).reduce((a, b) => a + b);
+  		// console.log(arr.slice(i, j));
+  		// console.log('gives temp:', currentTotal);
+  		
+  		if (currentTotal > maxValue) {
+  			maxValue = currentTotal;
+  		}
+  	}
+  }
+  return maxValue;
+}
+// TEST console.log('=20',maxSequence([-2, 1, 0,-4, 20]));
