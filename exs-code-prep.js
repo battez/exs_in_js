bb4d6d62716cae0122fe525f561197717318726e
battez <lukebarker@gmail.com>
2019-02-17 16:47:45 +0000

    add couple solutions to JS kata of codewars

--- exs-code-prep.js
+++ exs-code-prep.js
@@ -431,3 +431,83 @@ function queueTime(customers, n) {
   return time;
   
 }
+
+/*
+Write an algorithm that takes an array
+and moves all of the zeros to the end,
+preserving the order of the other elements.
+*/
+let moveZeros = function (arr) {
+	let result = [];
+	let zeros = 0;
+  for (let i = 0; i < arr.length; i++) {
+  	if(arr[i] !== 0) {
+  		result.push(arr[i]);
+  	} else {
+  		zeros++;
+  	}
+  }
+  while (zeros) {
+  	result.push(0);
+  	zeros--;
+  }
+  return result;
+}
+
+// tests
+// moveZeros([0,1,2,-1]); // [1,2,-1,0]
+// moveZeros([false,1,0,1,2,0,1,3,"a"]); // [false,1,1,2,1,3,"a",0,0]
+
+// smarter version using filter()
+let moveZerosAlt = function (arr) {
+	
+  return arr.filter(elm => elm !== 0).
+  					concat(arr.filter(elm => elm === 0));
+}
+
+
+/*Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive
+alphabetic characters and numeric digits that occur more than once in the
+input string. The input string can be assumed to contain only alphabets
+(both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice*/
+function duplicateCount(text){
+	
+	// although this uses two variables it is very clearly aligned with the logic
+	// of the task:
+  let once = new Set();
+  let duplicates = new Set();
+  for (char of text.toUpperCase()) {
+  	if (!once.has(char)) {
+  		once.add(char);
+  	} else {
+  		duplicates.add(char);
+  	}
+  }
+  return duplicates.size;
+}
+console.log(duplicateCount("Indivisibilities"));
+
+/*Same but squared:
+
+Given two arrays a and b write a function comp(a, b)
+that checks whether the two arrays have the "same" elements,
+with the same multiplicities.
+"Same" means, here, that the elements in b are the elements in a squared,
+regardless of the order.*/
+
+// tests
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]// same
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361] // fail
