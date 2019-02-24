5c111c2f0ad9aa39a71afaf0e870dd5329f15a02
battez <lukebarker@gmail.com>
2019-02-24 17:08:15 +0000

    add solution to domain name

--- exs-string.js
+++ exs-string.js
@@ -9,13 +9,25 @@ parses out just the domain name and returns it as a string. For example:
 
 function domainName(url){
 
-    // split on // remove left
-    let pieces = url.split('//');
+    // TODO: possibly add check / convert to lowercase?
+    
+    // split on protocol part
+    let pieces = url.split('://');
+    
+    // remove left if it had one
+    if(pieces.length > 1) {
+        pieces.shift();
+    }
+    
     // if substring 0,4 is www. remove
+    if(pieces[0].substring(0,4) === 'www.' ) {
+        console.log('Www');
+        pieces[0] = pieces[0].substring(4);
+    }
     
-    // split on . and take first element
-    
-    
+    // now should have the domain in the first section before a dot
+    pieces = pieces[0].split('.');
+    return pieces[0];
     
 }
 
