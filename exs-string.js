b9212134ccbd4c4bd016e6f199800be4be80f331
battez <lukebarker@gmail.com>
2019-02-25 14:29:51 +0000

    add a non-working solution for directions reduction puzzle. This solution does work to make a minimal path, but misinterpreted the task.

--- exs-string.js
+++ exs-string.js
@@ -35,3 +35,85 @@ function domainName(url){
 /*domainName("http://github.com/carbonfive/raygun");// == "github"
 domainName("http://www.zombie-bites.com"); // == "zombie-bites"
 domainName("https://www.cnet.com"); //== "cnet"*/
+
+/*Given the string representations of two integers, return the string
+representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+A string representation of an integer will contain no characters besides
+the ten numerals "0" to "9".*/
+
+// FIXME: this will work for trivial approach, but a more involved solution needed for passing codewars tests!
+function sumStrings(a,b) {
+    
+    // any empty strings:
+    if(a.trim().length === 0) {
+        a = 0;
+    }
+    
+    if(b.trim().length === 0) {
+        b = 0;
+    }
+    
+    
+    return (BigInt(a) + BigInt(b)).toString(10);
+    
+}
+console.log(sumStrings('123','456'));// 579
+console.log(sumStrings("123123123123123123123123123123123123123123123","456456456456456456456456456456456456456456456"));
+// 579579579579579579579579579579579579579579579
+
+/*
+Write a function dirReduc which will take an array of strings and returns an array of strings
+with the needless directions removed (W<->E or S<->N side by side).
+
+*/
+function dirReduc(arr){
+ 
+    let result = arr.reduce(function(accumulator, currentValue) {
+    
+        switch (currentValue) {
+          case 'NORTH':
+              accumulator[0] += 1;
+              break;
+          case 'SOUTH':
+              accumulator[0] -= 1;
+              break;
+          case 'EAST':
+              accumulator[1] += 1;
+              break;
+          default:
+              accumulator[1] -= 1;
+        }
+    
+        return accumulator;
+        
+    }, [0, 0]);
+
+
+    if(result[0] > 0) {
+        result[0] = Array(result[0]).fill('NORTH');
+    } else {
+        result[0] = Array(-1 * result[0]).fill('SOUTH');
+    }
+    
+    if(result[1] > 0) {
+        result[1] = Array(result[1]).fill('EAST');
+    } else {
+        result[1] = Array(-1 * result[1]).fill('WEST');
+    }
+    
+    result = result[0].concat(result[1]);
+    
+    console.log(result);
+    
+    return result;
+}
+
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]); //["WEST"]
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]);// => []
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);// ["WEST"]
+dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]); //["NORTH", "WEST", "SOUTH", "EAST"]
+dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]); // []
