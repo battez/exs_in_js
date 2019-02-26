f8a2a3c68ff0cd4711af875de32c055b958fb34a
battez <lukebarker@gmail.com>
2019-02-26 16:39:21 +0000

    add incomplete solution for string compare count task. It currently does counts and tracks letters needed for output in a Set()

--- exs-string.js
+++ exs-string.js
@@ -114,7 +114,7 @@ function dirReducMinimal(arr){
     return result;
 }
 
-// actual solution (in progress)
+// my solution to the actual Kata
 function dirReduc(arr){
  
     const complements = {
@@ -141,8 +141,84 @@ function dirReduc(arr){
     
     return result;
 }
-dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]); //["WEST"]
+/*dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]); //["WEST"]
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]);// => []
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);// ["WEST"]
 dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]); //["NORTH", "WEST", "SOUTH", "EAST"]
-dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]); // []
+dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]); // []*/
+
+
+/*
+Task: https://www.codewars.com/kata/5629db57620258aa9d000014/train/javascript
+
+Given two strings s1 and s2, we want to visualize how different the two strings are.
+We will only take into account the lowercase letters (a to z).
+First let us count the frequency of each lowercase letters in s1 and s2.
+
+non-regex solution attempt:
+*/
+function mix(s1, s2) {
+  
+  if(arguments.length !== 2) {
+      return '';
+  }
+  
+  let result = [];
+   
+  let counts = [new Map(), new Map()];
+  
+  // keep track of any letters used
+  let lettersUsed = new Set();
+  
+  for (let i = 0; i < counts.length; i++) {
+
+      // iterate from sentence i, finding frequency of letters
+      for (let chr of arguments[i]) {
+
+          // Only need 97 --> 122: i.e. lowercase letters a-z
+          if ((chr.charCodeAt(0) >= 97) && (chr.charCodeAt(0) <= 122)) {
+
+              counts[i].set(chr, 1 + counts[i].get(chr) || 1);
+              
+              // we only want any max's 2 or more
+              if(counts[i].get(chr) === 2) {
+                  lettersUsed.add(chr);
+              }
+          }
+
+      }
+      
+  }
+  
+  console.log(counts);
+  
+  console.log('used',lettersUsed);
+  for (let key of lettersUsed) {
+      
+  }
+
+  
+  // return result.join('/')
+}
+
+
+
+//tests
+mix("codewars", "codewars"); // ""
+mix("Lords of the Fallen", "gamekult");// "1:ee/1:ll/1:oo")
+/*mix("Are they here", "yes, they are here");// "2:eeeee/2:yy/=:hh/=:rr"
+mix("looping is fun but dangerous", "less dangerous than coding");// "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")
+mix(" In many languages", " there's a pair of functions");// "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")
+s1 = "my&friend&Paul has heavy hats! &"
+s2 = "my friend John has many many friends &"
+mix(s1, s2);//"2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+mix(s1, s2);//"1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+s1="Are the kids at home? aaaaa fffff"
+s2="Yes they are here! aaaaa fffff"
+mix(s1, s2);// "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"*/
+
+
